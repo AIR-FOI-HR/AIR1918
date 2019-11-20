0d67ad2192e91a92f9bcd3e4ee9e82bc7a76b661
Dario Šiprak <siprak214@hotmail.com>
2019-11-20 03:36:45 +0100

    Kreiraj activity za registraciju
    Kreirana je aktivnost za mogućnost registracije

--- 2Fit4U/app/src/main/java/hr/foi/a2fit4u/login/LoginActivity.java
+++ 2Fit4U/app/src/main/java/hr/foi/a2fit4u/login/LoginActivity.java
@@ -2,15 +2,40 @@ package hr.foi.a2fit4u.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import hr.foi.a2fit4u.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.edittext_username)
+    EditText mTextUsername;
+    @BindView(R.id.edittext_password)
+    EditText mTextPassword;
+
+    @OnClick(R.id.button_login)
+    void login(){
+        // TODO implement login
+    }
+
+    @OnClick(R.id.textview_register)
+    void openRegisterForm(){
+        // TODO implement register
+        Intent registerIntent = new Intent(LoginActivity.this,)
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
     }
 }
---
+++ 2Fit4U/app/src/main/java/hr/foi/a2fit4u/login/RegisterActivity.java
@@ -0,0 +1,16 @@
+package hr.foi.a2fit4u.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import hr.foi.a2fit4u.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
