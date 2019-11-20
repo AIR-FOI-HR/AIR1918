abfcc5a0cd7766e2b38f8b18b19fa656b31de8b5
Dario Šiprak <siprak214@hotmail.com>
2019-11-20 03:51:21 +0100

    Kreiraj elemente registracije
    Kreirani su elementi registracijskog prikaza
    
    Close #15

--- 2Fit4U/app/src/main/java/hr/foi/a2fit4u/login/RegisterActivity.java
+++ 2Fit4U/app/src/main/java/hr/foi/a2fit4u/login/RegisterActivity.java
@@ -3,14 +3,32 @@ package hr.foi.a2fit4u.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import hr.foi.a2fit4u.R;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    @BindView(R.id.edittext_username)
+    EditText mTextUsername;
+    @BindView(R.id.edittext_password)
+    EditText mTextPassword;
+    @BindView(R.id.edittext_cnf_password)
+    EditText mTextCnfPassword;
+
+    @OnClick(R.id.button_register)
+    void register(){
+        //TODO implement register
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        ButterKnife.bind(this);
     }
 }
