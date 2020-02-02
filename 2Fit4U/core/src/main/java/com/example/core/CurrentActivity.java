021792a453e4c4b75128c4fc6132a226fc2a23de
Dario Šiprak <dsiprak@foi.hr>
2020-02-02 09:26:13 +0100

    Implementiraj klasu CurrentActivity
    Implementirana je klasa CurrentActivity

--- 2Fit4U/core/src/main/java/com/example/core/CurrentActivity.java
+++ 2Fit4U/core/src/main/java/com/example/core/CurrentActivity.java
@@ -1,4 +1,16 @@
 package com.example.core;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class CurrentActivity {
+    private static AppCompatActivity activity;
+
+    public static void setActivity(AppCompatActivity activity)
+    {
+        CurrentActivity.activity = activity;
+    }
+
+    public static AppCompatActivity getActivity() {
+        return activity;
+    }
 }
