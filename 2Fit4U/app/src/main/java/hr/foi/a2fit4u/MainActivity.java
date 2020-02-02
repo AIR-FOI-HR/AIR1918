444ea7cdfdb21460ed4c5cb0fb9b1afc1a57353c
Robert Fejzulahu <rfejzulah@foi.hr>
2020-02-02 08:10:21 +0100

    Kreiraj MainActivity klasu
    Kreirana je klasa za navigaciju

---
+++ 2Fit4U/app/src/main/java/hr/foi/a2fit4u/MainActivity.java
@@ -0,0 +1,25 @@
+package hr.foi.a2fit4u;
+
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    //private Util util = new Util();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
+}
