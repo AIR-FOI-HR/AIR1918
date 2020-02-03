d73dd87f363309ddeef508f08d2d9c4bce0d9727
Marin Pehnec <mpehnec@foi.hr>
2020-02-03 11:01:52 +0100

    Kreiraj SettingsActivity klasu
    Kreirana je SettingsActivity klasa

---
+++ 2Fit4U/app/src/main/java/hr/foi/a2fit4u/settings/SettingsActivity.java
@@ -0,0 +1,14 @@
+package hr.foi.a2fit4u.settings;
+
+import android.content.SharedPreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+    }
+}
