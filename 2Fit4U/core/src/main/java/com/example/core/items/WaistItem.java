78ad8030ef55bbff2c2a9011313f9d4138b4ae9d
Dario Šiprak <dsiprak@foi.hr>
2020-02-03 03:05:01 +0100

    Implementiraj klasu WaistItem
    Implementirana je klasa WaistItem

--- 2Fit4U/core/src/main/java/com/example/core/items/WaistItem.java
+++ 2Fit4U/core/src/main/java/com/example/core/items/WaistItem.java
@@ -1,4 +1,28 @@
 package com.example.core.items;
 
 public class WaistItem {
+
+    private String waistValue;
+    private String dateValue;
+
+    public WaistItem(String waistValue, String dateValue) {
+        this.waistValue = waistValue;
+        this.dateValue = dateValue;
+    }
+
+    public String getWaistValue() {
+        return waistValue;
+    }
+
+    public void setWaistValue(String waistValue) {
+        this.waistValue = waistValue;
+    }
+
+    public String getDateValue() {
+        return dateValue;
+    }
+
+    public void setDateValue(String dateValue) {
+        this.dateValue = dateValue;
+    }
 }
