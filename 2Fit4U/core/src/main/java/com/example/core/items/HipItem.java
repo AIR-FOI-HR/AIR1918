cad909e13f2064db2d581cbe564e13ae4dc8a4ed
Dario Šiprak <dsiprak@foi.hr>
2020-02-03 03:01:01 +0100

    Implementiraj klasu HipItem
    Implementirana je klasa HipItem

--- 2Fit4U/core/src/main/java/com/example/core/items/HipItem.java
+++ 2Fit4U/core/src/main/java/com/example/core/items/HipItem.java
@@ -1,4 +1,28 @@
 package com.example.core.items;
 
 public class HipItem {
+
+    private String hipValue;
+    private String dateValue;
+
+    public HipItem(String hipValue, String dateValue) {
+        this.hipValue = hipValue;
+        this.dateValue = dateValue;
+    }
+
+    public String getHipValue() {
+        return hipValue;
+    }
+
+    public void setHipValue(String hipValue) {
+        this.hipValue = hipValue;
+    }
+
+    public String getDateValue() {
+        return dateValue;
+    }
+
+    public void setDateValue(String dateValue) {
+        this.dateValue = dateValue;
+    }
 }
